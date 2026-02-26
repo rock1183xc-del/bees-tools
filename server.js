adc9eab2c5871b34e0e2f16734fa647cccfaed4c
rock1183xc-del <rock1183xc@gmail.com>
2026-02-26 11:12:38 +0800

    server.js 匯入 express 並明確處理首頁 /

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
-// 此檔案僅為 Vercel 相容性預留，實際邏輯在 api/tools.js 與靜態檔
-// 本機開發請執行：npm run dev（使用 local-dev.js）
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const isVercel = !!process.env.VERCEL;
+
+app.use(cors());
+app.use(express.json());
+
+if (isVercel) {
+  const apiTools = require('./api/tools.js');
+  app.all('/api/tools', (req, res) => apiTools(req, res));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+  app.use(express.static(__dirname));
+}
+
+module.exports = app;
